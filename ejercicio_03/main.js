385b24d6e89658296bb4ae5b9a3a9ffbd374f981
Ched2370 <mmibz.85@gmail.com>
2024-02-11 23:41:51 -0300

    subiendo ejerciccio 3 terminado

---
+++ ejercicio_03/main.js
@@ -0,0 +1,38 @@
+var btn = document.getElementById('btn');
+btn.addEventListener("click", solucion);
+
+const r = document.getElementById('respuesta');
+
+function solucion(){
+
+    var lista = new Array('a', 'b', 'c', '1', 'x', '9', 'd', '4');
+    var item;
+    
+    for (item of lista) {
+        if (item === '1' || item === '9' || item === '4'){
+            continue;
+        } else {
+            console.log(item);
+        }
+    }
+
+    r.innerHTML = "var lista = new Array('a','b','c','1','x','9','d','4');" + "<br>" +
+    "var item;" + "<br>" +
+    "" + "<br>" +
+    "for (item of lista) {" + "<br>" +
+    "    if (item === '1' || item === '9' || item === '4'){" + "<br>" +
+    "        continue;" + "<br>" +
+    "" + "<br>" +
+    "    } else {" + "<br>" +
+    "        console.log(item);" + "<br>" +
+    "    }};";
+}
+
+
+
+
+
+
+
+
+
