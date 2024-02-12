944289651e8caaa745d275766b6fe19f4d4fa1ba
Ched2370 <mmibz.85@gmail.com>
2024-02-12 17:34:17 -0300

    ejercicio 5 terminado

---
+++ ejercicio_05/main.js
@@ -0,0 +1,36 @@
+const r = document.getElementById('respuesta');
+const click = document.getElementById("btn");
+click.addEventListener('click', jugar);
+
+num = Math.floor(Math.random() * 100 ** 3 + 1);
+
+function jugar() {
+  try {
+    console.log(num);
+    r.innerText = 'mmm';
+    let contador = 0;
+    let a;
+    do {
+      if (contador === 0) {
+        a = prompt("Adivina el numero entre 1 y 1000000");
+      } else {
+        if (a > num) {
+          a = prompt("El numero es mas pequeño, intentalo de nuevo");
+        }
+        if (a < num) {
+          a = prompt("El numero es mas grande, intentalo de nuevo");
+        }
+      }
+      contador++;
+      console.log(contador);
+    } while (a != num);
+    a = alert("FELICIDADES!!!, HAS DESCUBIERTO EL NUMERO");
+  } catch (err) {
+    alert("a surjido un error " + err.message);
+  }finally {
+    console.log(num);
+    r.innerHTML = 'FELICITACIONES!!!' + '<br>' +
+    'el numero era ' + num;
+    num = Math.floor(Math.random() * 100 ** 3 + 1);
+  }
+}
