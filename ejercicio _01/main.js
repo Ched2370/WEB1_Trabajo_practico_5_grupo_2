7e81180c9879c3cc5f4592ca9983a0dc942afa0d
Ched2370 <mmibz.85@gmail.com>
2024-02-11 21:55:34 -0300

    eliminado etiqueta <form> que genera error

--- ejercicio _01/main.js
+++ ejercicio _01/main.js
@@ -3,13 +3,16 @@ boton.addEventListener("click", calcularDiasVividos);
 
 function calcularDiasVividos() {
     try {
+        
         var nombre = prompt('Ingrese su nombre');
         var edad = prompt('Ingrese su edad');
         var diasVividos = edad * 365;
 
         var resultado = nombre + ' ha vivido aproximadamente ' + diasVividos + ' días.';
         document.getElementById('res').innerText = resultado;
+
         console.log(resultado);
+
     } catch(err) {
         alert("Ha ocurrido un error: " + err.message);
     }
