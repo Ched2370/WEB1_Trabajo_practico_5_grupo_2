dd43ad2b13a284d3b5da4b35cedf7e306e3bd207
Ched2370 <mmibz.85@gmail.com>
2024-02-16 18:24:20 -0300

    ejercicio 11 Terminado

---
+++ Practica A/ejercicio_11/main.js
@@ -0,0 +1,35 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+const btn = document.getElementById('btn');
+btn.addEventListener('click', mostrar);
+
+const resp = document.getElementById('resDia');
+
+function mostrar() {
+    try {
+        var varDay = document.getElementById('dias').value;
+        var varMonth = document.getElementById('mes').value;
+        var indiceMes = meses.indexOf(varMonth);
+        var varYear = document.getElementById('año').value;
+
+        console.log(varDay);
+        console.log(varMonth);
+        console.log(varYear);
+
+        let patron = /^\d{4}$/;
+        if(patron.test(varYear)){
+            console.log('año correecto ' + varYear);
+            var fecha = new Date(varYear, indiceMes, varDay);
+            resp.innerText = fecha.toDateString();
+            console.log(fecha);
+        }else {
+            console.log('año invalido ' + varYear);
+            alert('el año no es valido');
+        }
+    } catch (err) {
+        alert('Ha sujido un error ' + err.message);
+    }
+}
+
+function esAñoBisiesto(){
+    return (año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0);
+}
