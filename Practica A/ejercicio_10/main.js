64a2f6f736effc605d8e60992d275ebc4d9507ec
Ched2370 <mmibz.85@gmail.com>
2024-02-16 15:52:34 -0300

    reto del profe ejercicio 10 -hacerlo con un input text en lugar de un input data-

--- Practica A/ejercicio_10/main.js
+++ Practica A/ejercicio_10/main.js
@@ -1,25 +1,34 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', calcularDias);
 
-const fecha = document.getElementById('calendario');
 const respuesta = document.getElementById('respuesta');
 
 function calcularDias() {
-  try{
-  var fechaSeleccionada = new Date(fecha.value);
-  var fechaActual = new Date();
-  var diferencia = fechaSeleccionada - fechaActual;
-  var dias = Math.ceil(diferencia / (1000 * 60 * 60 * 24));// conversion de milisegundos a dias
-  if(dias == 0){
-    respuesta.innerText = 'Hoy es un gran dia!';
-  }else if(fechaSeleccionada < fechaActual){
-    respuesta.innerText = 'Han transcurrido ' + Math.abs(dias) + ' dias';
-  }else if(fechaSeleccionada > fechaActual){
-    respuesta.innerText = 'Faltan ' + dias + ' dias';
-  }else {
-    alert('No has ingresado una fecha,\nvuelve a intentarlo');
+  try {
+    var fecha = document.getElementById('calendario').value;
+    var fechaP = [];
+    fechaP = fecha.split('/');
+    var fechaS = new Date(
+      fechaP[2],
+      fechaP[1] - 1,
+      fechaP[0]
+    );
+    var fechaA = new Date();
+    console.log(fechaS);
+    console.log(fechaA);
+    var dif = fechaS - fechaA;
+    var dias = Math.ceil(dif / (1000 * 60 * 60 * 24)); // conversion de milisegundos a dias
+    if (dias == 0) {
+      respuesta.innerText = 'Hoy es un gran dia!';
+    } else if (fechaS < fechaA) {
+      respuesta.innerText = 'Han transcurrido ' + Math.abs(dias) + ' dias';
+    } else if (fechaS > fechaA) {
+      respuesta.innerText = 'Faltan' + dias + ' dias';
+    } else {
+      fecha.value = '';
+      alert("Fecha invalida");
+    }
+  } catch (err) {
+    alert("Ha surjido un error " + err.message);
   }
-}catch(err){
-  alert('Ha surjido un error ' + err.message);
-}
 }
