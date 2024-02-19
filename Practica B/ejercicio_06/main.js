cde805bbe08a98f3532ea65ac6ea3162cb43d062
Ched2370 <mmibz.85@gmail.com>
2024-02-19 18:13:17 -0300

    ejercicio 6B Terminado

---
+++ Practica B/ejercicio_06/main.js
@@ -0,0 +1,20 @@
+const style = document.getElementById('style');
+
+function rojo(){
+    style.innerHTML = 'body {background-color: red;}';
+}
+function azul(){
+    style.innerHTML = 'body {background-color: blue;}';
+}
+function blanco(){
+    style.innerHTML = 'body {background-color: white;}';
+}
+function amarillo(){
+    style.innerHTML = 'body {background-color: yellow;}';
+}
+function verde(){
+    style.innerHTML = 'body {background-color: green;}';
+}
+function salida() {
+    style.innerHTML = 'body {background-color: black;}';
+}
