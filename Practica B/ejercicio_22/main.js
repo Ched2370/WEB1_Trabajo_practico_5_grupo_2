6403f9c0a6d2173558ee6a0433db36b4f40bc119
Ched2370 <mmibz.85@gmail.com>
2024-02-24 06:57:51 -0300

    agregando emoticones al ejercicio 22B

--- Practica B/ejercicio_22/main.js
+++ Practica B/ejercicio_22/main.js
@@ -1,14 +1,17 @@
+const emoti = ['&#128539', '&#128548', '&#128552', '&#128554', '&#128536', '&#128545', '&#128560', '&#128561', '&#128565', '&#129326'];
 const btn = document.getElementById('btn');
 btn.addEventListener('mouseover', escapar);
 btn.addEventListener('click', ganaste);
 
 function escapar() {
     try {
+        let emoji = Math.floor(Math.random() * (emoti.length));
         let coordX = Math.floor(Math.random() * 80) + 1;
         let coordY = Math.floor(Math.random() * 90) + 1;
         btn.style = 
         'top: ' + coordY + 'vh;\n' +
         'left: ' + coordX + 'vw;';
+        btn.innerHTML = emoti[emoji];
     } catch (err) {
         alert('algo a salido mal ' + err.message);
     }
