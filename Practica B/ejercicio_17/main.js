58806a19e96e4342e80e50f99980a76eeb79721a
Ched2370 <mmibz.85@gmail.com>
2024-02-27 20:52:11 -0300

    ejercicio 17 moodificado en clase por un setInterval()

--- Practica B/ejercicio_17/main.js
+++ Practica B/ejercicio_17/main.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', comprobar);
 
-function comprobar() {
+/*function comprobar() {
     try {
         const frase = document.getElementById('fraseIngresada').value;
         const respuesta = document.getElementById('respuesta');
@@ -18,4 +18,25 @@ function comprobar() {
     } catch (err) {
         swal('ERROR', 'Ha surgido un error ' + err.message, 'error');
     }
+}*/
+
+function comprobar() {
+    try {
+        let indice = 0;
+        const frase = document.getElementById('fraseIngresada').value;
+        const respuesta = document.getElementById('respuesta');
+
+        let a = setInterval(() => {
+            respuesta.innerHTML += frase.charAt(indice);
+            indice ++;
+            if (indice == frase.length) {
+                clearInterval(a);
+            }
+        }, 700);
+        frase.value = '';
+        respuesta.innerHTML += '<br>';
+    } catch (err) {
+        
+    }
+
 }
